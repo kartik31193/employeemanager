008c63a56a71b99923bd29b56715ab3b59a76ec4
kartik <kartik31193@gmail.com>
2023-04-23 20:11:26 +0530

    added model, repo, service, controller

---
+++ src/main/java/com/kartikapps/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kartikapps.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/kartikapps/employeemanager/model/Employee.java
+++ src/main/java/com/kartikapps/employeemanager/model/Employee.java
@@ -1,11 +1,25 @@
 package com.kartikapps.employeemanager.model;
 
-public class Employee {
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String email;
     private String jobTitle;
     private String phone;
     private String imageUrl;
+    @Column(nullable = false, updatable = false)
     private String employeeCode;
+
 }
