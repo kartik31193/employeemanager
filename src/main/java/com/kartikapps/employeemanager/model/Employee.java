9e8db8642df758304b18458f71133b4bf5631153
kartik <kartik31193@gmail.com>
2023-04-23 18:12:58 +0530

    Initial commit

---
+++ src/main/java/com/kartikapps/employeemanager/model/Employee.java
@@ -0,0 +1,11 @@
+package com.kartikapps.employeemanager.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+}
